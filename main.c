eda78f1fa9cebf589e118b843a10af8c14634ee0
Ic3Venom <Ic3Venom@gmail.com>
2016-08-26 09:50:32 -0700

    Update main.c

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ uint64_t exponentiate(short base, short exponent);
 main()
 {
     uint64_t num, *pNum;
-    unsigned short oldBase, newBase;
+    short oldBase, newBase;
 
     std::cout << "What base is the original number? (MIN is 2, MAX is 16)\n$ ";
     std::cin >> oldBase;
     baseInRange(oldBase);
 
-    std::cout << "What number do you want converted? (MIN is 1, MAX is (2^64-1)-10)\n$ ";
+    std::cout << "What number do you want converted? (MIN is 0, MAX is (2^64-1)-10)\n$ ";
     pNum = &num;
     num = translateToDecimal(pNum, oldBase);
 
@@ -33,7 +33,7 @@ main()
 //It repeats twice, so why not a function?
 void baseInRange(short base)
 {
-    if (base > 16 || base <= 1)
+    if (base < 1 || base > 16)
     {
         std::cout << "Expected base case in range of 2 to 16, got " << base << ". Exiting program...";
         exit(base);
@@ -42,21 +42,22 @@ void baseInRange(short base)
 //Changes bases 2-16 into base 10 for easier conversion
 uint64_t translateToDecimal(uint64_t *pNum, short oldBase)
 {
-    uint64_t decimal = 0;
+    uint64_t decimal= 0;
     std::string num;
 
     std::cin >> num;
     //For each digit in num from index 0 to num.length
-    for (short column(0); column <= num.length() - 1; column++)
+    for (short column(0); column <= num.length() -1; column++)
     {
         //For each possible number oldBase can hold
-        for (short possibleNumber(oldBase-1), error(0); possibleNumber >= 0; possibleNumber--)
+        for (short possibleNumber(oldBase -1), error(0); possibleNumber >= 0; possibleNumber--)
         {
             std::string numbers = "0123456789ABCDEF";
+            
             if (numbers[possibleNumber] == num[column])
             {
                 //Long way to say (Base ^ Column+1) * pN, which finds the value of each column in decimal
-                decimal += exponentiate(oldBase, num.length()-(column+1)) * possibleNumber;
+                decimal+= exponentiate(oldBase, num.length()-(column +1)) * possibleNumber;
                 break;
             }
             //Exit program if number typed is not a number or is not a number range of oldBase
@@ -79,31 +80,37 @@ void convert(uint64_t *pNum, short newBase)
     short length= 0;
     std::string newNum;
 
-    uint64_t woweeeee = *pNum;
-    //Column can be described using examples: column[0] of 123 is 3 / column[1] of 5791 is 9
-    for(short column(0); woweeeee > 0; column++)
+    //When oldNum is 0
+    if(!*pNum)
+    {
+        std::cout << 0;
+        return;
+    }
+    
+    //Column can be described using examples: column[0] of 123 is 3, column[1] of 5791 is 9
+    for(short column(0); *pNum > 0; column++)
     {
         //Max range of oldNum
-        uint64_t maxRange = exponentiate(newBase, column) -1;
+        uint64_t maxRange= exponentiate(newBase, column) -1;
 
-        if (maxRange >= woweeeee)
+        if (maxRange >= *pNum)
         {
             //Min range of oldNum
             uint64_t minRange = exponentiate(newBase, column -1);
 
             //Loops to see how many times minRange can go into oldNum column amount of times before passing maxRange
-            for(short possibleNumber = newBase-1; possibleNumber >= 0; possibleNumber--)
+            for(short possibleNumber(newBase -1); possibleNumber >= 0; possibleNumber--)
             {
-                if ((minRange * possibleNumber) <= woweeeee && (minRange * possibleNumber) <= maxRange)
+                if ((minRange * possibleNumber) <= *pNum)
                 {
                     if(!length)
                     {
-                        length = column -1;
+                        length= column -1;
                     }
                     std::string numbers = "0123456789ABCDEF";
                     //Puts correct number into correct column
-                    newNum[column]= numbers[possibleNumber];
-                    woweeeee-= (minRange * possibleNumber);
+                    newNum[column -1]= numbers[possibleNumber];
+                    *pNum-= (minRange * possibleNumber);
                     column= 0;
 
                     break;
@@ -113,10 +120,9 @@ void convert(uint64_t *pNum, short newBase)
         //Column is given '0' because it will not hold anything or anything yet
         else
         {
-            newNum[column-1] = '0';
+            newNum[column -1]= '0';
         }
     }
-
     //Prints 'column' amount of times
     for(;length >= 0; length--)
     {
@@ -128,11 +134,11 @@ void convert(uint64_t *pNum, short newBase)
 //Also exponentiate is the correct term of the process of bringing x to the power of y
 uint64_t exponentiate(short base, short exponent)
 {
-    uint64_t power = 1;
+    uint64_t power= 1;
 
     for (;exponent >= 1; exponent--)
     {
-        power *= base;
+        power*= base;
     }
     return power;
 }
