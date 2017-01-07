57e5e27a9542e6cc4df536c68abb77a8bb8c4b89
Ic3Venom <Ic3Venom@gmail.com>
2017-01-06 19:51:54 -0800

    Code etiquette
    Changed varuable names around and started on the 'convertBase' function

--- main.c
+++ main.c
@@ -5,96 +5,76 @@
 
 //Purpose: to convert numbers in base-(2->16) into number base-(2->16)
 //Created by Julian Meyn August 23, 2016; REMASTERED(again) January 5, 2017
-void        baseInRange         (short base);
-uint64_t    translateToDecimal  (std::string oldNum, short oldBase);
-std::string convert             (uint64_t *pNum, short newBase);
-int         stringToInt         (char character);
-uint64_t    exponentiate        (short base, short exponent);
+
+uint64_t    convertDecimal (std::string num , short baseOld );
+void        baseInRange    (short       base);
+std::string convertBase    (uint64_t    num , short baseNew );
+uint64_t    exponentiate   (short       base, short exponent);
+int         stringToInt    (char        chr );
 
 int main()
 {
-    uint64_t numDecimal, *pNum;
-    short oldBase, newBase;
-    pNum = &numDecimal;
-    std::string oldNum, newNum;
+    uint64_t 	numInt ,*pNumInt;
+    short 		baseOld, baseNew;
+    std::string numStr;
 
-    std::cout << "First base" << std::endl;
-    std::cin >> oldBase;
-    baseInRange(oldBase);
+    std::cout << "First base:\n$ ";
+    std::cin >> baseOld;
+    baseInRange(baseOld);
 
-    std::cout << "First number" << std::endl;
-    std::cin >> oldNum;
-    numDecimal = translateToDecimal(oldNum, oldBase);
+    std::cout << "First number:\n$ ";
+    std::cin >> numStr;
+    numInt = convertDecimal(numStr, baseOld);
 
-    std::cout << numDecimal;
-    /*std::cout << "New base" << std::endl;
-    std::cin >> newBase;
-    baseInRange(newBase);
+    std::cout << "New base:\n$ ";
+    std::cin >> baseNew;
+    baseInRange(baseNew);
 
-    //newNum = static_cast<std::string>(convert(pNum, newBase));
-    */
+    pNumInt = &numInt;
+    convertBase(*pNumInt, baseNew);
+
     return 1;
 }
 
-//It repeats twice, so why not a function?
-void baseInRange(short base)
+//converts bases 2-16 into base 10 for easier conversion later
+uint64_t convertDecimal(std::string num, short baseOld)
 {
-    if (base < 2 || base > 16)
-    {
-        std::cout << "Expected base case in range of 2 to 16, got " << base << ". Exiting program...";
-        exit(1);
-    }
-}
+    uint64_t    numInt      {0};
+    short       indexInt    {0};
+    std::string numberRange {"0123456789ABCDEF"};
 
-//Changes bases 2-16 into base 10 for easier conversion
-uint64_t translateToDecimal(std::string oldNum, short oldBase)
-{
-    uint64_t    decimal {0};
-    int         indexInt{0};
-    std::string numbers {"0123456789ABCDEF"};
-
-    for (short i{0}; i < oldNum.length(); i++)
+    for (short i{0}; i < num.length(); i++)
     {
         //Checks if 'oldNum' contains numbers that are in its base capacity
-        for(short j{oldBase}; j < 0; j--)
+        for(short j{baseOld}; j < 0; j--)
         {
-            if (oldNum[i] == numbers[j])
+            if (num[i] == numberRange[j])
             {
-                std::cout << "Unknown value '" << oldNum[i] << "' in base " << oldBase << ". Exiting program..." << std::endl;
+                std::cout << "Unknown value '" << num[i] << "' in base " << baseOld << ". Exiting program..." << std::endl;
                 exit(1);
             }
         }
-        indexInt = stringToInt(oldNum[i]);
+
+        indexInt = stringToInt(num[i]);
+
         //Formula used: y = x * b^i; can find the value of any digit in a number of base-b
-        decimal += (indexInt * exponentiate(oldBase, (oldNum.length()-1 - i)));
+        numInt += (indexInt * exponentiate(baseOld, (num.length() - 1 - i)));
     }
 
-    /*//For each digit in oldNum from index 0 to oldNum.length
-    for (short column = 0; column <= oldNum.length() -1; column++)
-    {
-        //For each possible number oldBase can hold
-        for (short possibleNumber = oldBase - 1, error(0); possibleNumber >= 0; possibleNumber--)
-        {
-            if (numbers[possibleNumber] == oldNum[column])
-            {
-                //Long way to say (Base ^ Column+1) * pN, which finds the value of each column in decimal
-                decimal += (exponentiate(oldBase, oldNum.length() - (column + 1)) * possibleNumber);
-                break;
-            }
-            //Exit program if number typed is not a number or is not a number range of oldBase
-            else
-            {
-                error++;
+    return numInt;
+}
 
-                if (error == oldBase)
-                {
-                    std::cout << "Unknown value '" << oldNum[column] << "' in base " << oldBase << ". Exiting program..." << std::endl;
-                    exit(1);
-                }
-            }
-        }
-    }*/
-    return decimal;
+//Converts number from convertDecimal into base-(2->16)
+std::string convertBase(uint64_t num, short baseNew)
+{
+    std::string numNew;
+    std::string numSave;
+
+    std::cout << num;
+    for(short i = 0;; i++)
+    {
+        exit(0);
+    }
 }
 
 /*//Terrible function main, but it is the most important conversion
@@ -143,34 +123,41 @@ std::string conversion(uint64_t *pNum, short newBase)
     return static_cast<std::string>(newNum);
 }*/
 
-//From another program of mine, 'ultamitetypingtest'...
-int stringToInt(char character)
+//Checks if number given is within 2 and 16
+void baseInRange(short base)
 {
-    switch(character);
+    if (base < 2 || base > 16)
     {
-        //This was the only way that I could think of myself, StackOverflow said to use\
-        //vectors, which seems over-complicated for such a simple task
-        switch (character)
-        {
-        case 49: return 1;
-        case 50: return 2;
-        case 51: return 3;
-        case 52: return 4;
-        case 53: return 5;
-        case 54: return 6;
-        case 55: return 7;
-        case 56: return 8;
-        case 57: return 9;
-        //and then some
-        case 65: return 10;
-        case 66: return 11;
-        case 67: return 12;
-        case 68: return 13;
-        case 69: return 14;
-        case 70: return 15;
-        //and then back to the regularly scheduled 'program'
-        default: return 0;
-        }
+        std::cout << "Expected base case in range of 2 to 16, got " << base << ". Exiting program...";
+        exit(1);
+    }
+}
+
+//From another program of mine, 'ultamitetypingtest'...
+int stringToInt(char chr)
+{
+    //This was the only way that I could think of myself, StackOverflow said to use\
+    //vectors, which seems over-complicated for such a simple task
+    switch (chr)
+    {
+    case 49: return 1;
+    case 50: return 2;
+    case 51: return 3;
+    case 52: return 4;
+    case 53: return 5;
+    case 54: return 6;
+    case 55: return 7;
+    case 56: return 8;
+    case 57: return 9;
+    //and then some
+    case 65: return 10;
+    case 66: return 11;
+    case 67: return 12;
+    case 68: return 13;
+    case 69: return 14;
+    case 70: return 15;
+    //and then back to the regularly scheduled 'program'
+    default: return 0;
     }
 }
 
@@ -182,12 +169,12 @@ uint64_t exponentiate(short base, short exponent)
 
     if(exponent == 0)
     {
-        return 1;
+        return power;
     }
 
     for (;exponent >= 1; exponent--)
     {
-        power*= base;
+        power *= base;
     }
     return power;
 }
